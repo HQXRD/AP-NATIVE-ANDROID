280c775ca321c2ea27d955294b6e353403bd06f3
Brunithi <Brunithi>
2023-12-01 18:55:14 +0800

    钱包界面完成

--- module-mine/src/main/java/com/xtree/mine/ui/viewmodel/MyWalletViewModel.java
+++ module-mine/src/main/java/com/xtree/mine/ui/viewmodel/MyWalletViewModel.java
@@ -7,8 +7,10 @@ import androidx.annotation.NonNull;
 import com.xtree.mine.data.MineRepository;
 
 import me.xtree.mvvmhabit.base.BaseViewModel;
+import me.xtree.mvvmhabit.bus.event.SingleLiveData;
 
 public class MyWalletViewModel extends BaseViewModel<MineRepository> {
+    public SingleLiveData<String> itemClickEvent = new SingleLiveData<>();
     public MyWalletViewModel(@NonNull Application application, MineRepository repository) {
         super(application, repository);
     }
