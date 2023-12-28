ee702b49e04ff12d9f23aacd34ac80901fef2ace
ace236 <accee236@gmail.com>
2023-12-28 17:57:22 +0800

    我的 增加账户管理弹窗

---
+++ module-mine/src/main/java/com/xtree/mine/ui/fragment/AccountMgmtDialog.java
@@ -0,0 +1,70 @@
+package com.xtree.mine.ui.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.lxj.xpopup.core.BottomPopupView;
+import com.lxj.xpopup.util.XPopupUtils;
+import com.xtree.base.router.RouterFragmentPath;
+import com.xtree.mine.R;
+import com.xtree.mine.databinding.DialogAccountMgmtBinding;
+
+import me.xtree.mvvmhabit.base.ContainerActivity;
+
+/**
+ * 账户管理 底部弹窗
+ */
+public class AccountMgmtDialog extends BottomPopupView {
+
+    Context ctx;
+    DialogAccountMgmtBinding binding;
+
+    public AccountMgmtDialog(@NonNull Context context) {
+        super(context);
+        ctx = context;
+    }
+
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        binding = DialogAccountMgmtBinding.bind(findViewById(R.id.cl_root));
+        initView();
+    }
+
+    private void initView() {
+        binding.ivwClose.setOnClickListener(v -> dismiss());
+
+        // 这里加点击事件
+        for (int i = 0; i < binding.llMenu.getChildCount(); i++) {
+            View child = binding.llMenu.getChildAt(i);
+            child.setOnClickListener(v -> {
+                String type = child.getTag().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("type", type);
+                String path = RouterFragmentPath.Mine.PAGER_SECURITY_VERIFY_CHOOSE;
+                //ctx.startContainerFragment(RouterFragmentPath.Mine.PAGER_SECURITY_VERIFY_CHOOSE, bundle);
+
+                Intent intent = new Intent(getContext(), ContainerActivity.class);
+                intent.putExtra(ContainerActivity.ROUTER_PATH, path);
+                intent.putExtra(ContainerActivity.BUNDLE, bundle);
+                ctx.startActivity(intent);
+                dismiss();
+            });
+        }
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.dialog_account_mgmt;
+    }
+
+    @Override
+    protected int getMaxHeight() {
+        //return super.getMaxHeight();
+        return (XPopupUtils.getScreenHeight(getContext()) * 85 / 100);
+    }
+}
