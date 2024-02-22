e72c065f2a9fd3ea0a3e8f3901476c4279b54a11
hqxc-mobius <hqxc.mobius@gmail.com>
2024-02-22 10:59:08 +0800

    登入页面 密码存取使用加密方案

--- module-mine/src/main/java/com/xtree/mine/data/Spkey.java
+++ module-mine/src/main/java/com/xtree/mine/data/Spkey.java
@@ -5,5 +5,5 @@ public class Spkey {
     public static final String REMEMBER_PWD = "remember_pwd";
     public static final String PWD = "pwd_key";
     public static final String ACCOUNT = "account_key";
-
+    public static final String KEY = "key";
 }
