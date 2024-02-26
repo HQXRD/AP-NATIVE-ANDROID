f3cba115f11c430f09b9fcf04d011b5d851f9410
hqxc-mobius <hqxc.mobius@gmail.com>
2024-02-26 10:55:33 +0800

    公用组件 修改浮动视窗的接口

--- library-base/src/main/java/com/xtree/base/adapter/CacheViewHolder.java
+++ library-base/src/main/java/com/xtree/base/adapter/CacheViewHolder.java
@@ -1,11 +1,14 @@
 package com.xtree.base.adapter;
 
+import android.os.CountDownTimer;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CacheViewHolder extends RecyclerView.ViewHolder implements LayoutContainer {
+    public CountDownTimer countDownTimer; // 2024/02/26 此为倒数计时，在遇到需要单
+
     public CacheViewHolder(@NonNull View itemView) {
         super(itemView);
     }
