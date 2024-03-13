89445958838c2cd6fd3be3e6a3f385a52c1b8b97
hqxc-kaka <hqxc.kaka@gmail.com>
2024-03-13 17:37:01 +0800

    返水契约 HttpDataSource

---
+++ module-mine/src/main/java/com/xtree/mine/data/source/APIManager.java
@@ -0,0 +1,12 @@
+package com.xtree.mine.data.source;
+
+/**
+ * Created by KAKA on 2024/3/12.
+ * Describe: api 静态管理
+ */
+public class APIManager {
+
+    public static final String GAMEREBATEAGRT_URL = "/compact/usersport/";
+    public static final String GAMESUBORDINATEAGRTE_URL = "/compact/teamcompactlive/";
+    public static final String GAMESUBORDINATEREBATE_URL = "/compact/teamsalarylive/";
+}
