80430b5e66d6cc6a551a5eb663454502c7b08ef2
hqxc-mobius <hqxc.mobius@gmail.com>
2024-03-15 09:28:02 +0800

    主页 修复场馆导览列乱跑问题

--- module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
+++ module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
@@ -81,6 +81,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
     private int clickCount = 0; // 点击次数 debug model
     private boolean selectUpdate;//手动更新余额
     private UpdateVo updateVo;//更新
+    private boolean isSelectedGame = false;
 
     @Override
     public int initContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -108,7 +109,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         viewModel.readCache(); // 读取缓存,用户信息可能发生了变更
         TagUtils.tagDailyEvent(getContext());
         checkUpdate(); // 检查更新
-        if(!TextUtils.isEmpty(token)) {
+        if (!TextUtils.isEmpty(token)) {
             checkRedPocket();
         }
     }
@@ -193,6 +194,8 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
             //KLog.d(list.get(0));
             gameAdapter.clear();
             gameAdapter.addAll(list);
+            RadioButton rBtn = (RadioButton) binding.rgpType.getChildAt(0);
+            rBtn.setChecked(true);
         });
 
         viewModel.liveDataPlayUrl.observe(getViewLifecycleOwner(), map -> {
@@ -413,13 +416,17 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         binding.rcvList.setAdapter(gameAdapter);
         manager = new LinearLayoutManager(getContext());
         binding.rcvList.setLayoutManager(manager);
-
         binding.rcvList.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
-                if (needScroll == true && (newState == RecyclerView.SCROLL_STATE_IDLE)) {
+                // 当他划到找到要的item，将此item移到最上方
+                if (needScroll && (newState == RecyclerView.SCROLL_STATE_IDLE)) {
                     scrollRecycleView();
+                }
+                // 当他滑动时才让radioButton可以控制
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    isSelectedGame = false;
                 }
             }
 
@@ -432,7 +439,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
                     return;
                 }
                 GameVo vo = gameAdapter.get(position);
-                if (vo.pId != curPId) {
+                if (vo.pId != curPId && !isSelectedGame) {
                     curPId = vo.pId;
                     RadioButton rbtn = binding.rgpType.findViewWithTag("tp_" + curPId);
                     rbtn.setChecked(true);
@@ -452,6 +459,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
             rbtn.setButtonDrawable(dr);
 
             rbtn.setOnClickListener(v -> {
+                isSelectedGame = true;
                 String tag = v.getTag().toString();
                 int pid = Integer.parseInt(tag.replace("tp_", ""));
                 smoothToPosition(pid);
