44876041a81de99a96287dec79e1e4e6c7a4eec8
hqxc-ace <hqxc.ace@gmail.com>
2024-03-18 12:00:18 +0800

    盈亏报表 改版面,增加本页合计,总合计

--- module-mine/src/main/java/com/xtree/mine/vo/ProfitLossVo.java
+++ module-mine/src/main/java/com/xtree/mine/vo/ProfitLossVo.java
@@ -6,18 +6,18 @@ package com.xtree.mine.vo;
 public class ProfitLossVo {
 
     public String userid; // "2888826",
-    public String sbet; // "73498.9060",
-    public String spoint; // "0.0000",
+    public String sbet; // "73498.9060", 总投注
+    public String spoint; // "0.0000", 返点
     public String srake; // "0.3000",
-    public String seffective_bet; // "58535.1900",
-    public String sprize; // 56239.6234,
-    public String sprofit_loss; // -17259.2826,
-    public String sactive_prize; // "166.0000",
-    public String wages; // "215.0000",
-    public String fee; // 0,
+    public String seffective_bet; // "58535.1900", 有效投注
+    public String sprize; // 56239.6234, 中奖金额
+    public String sprofit_loss; // -17259.2826, 会员输赢
+    public String sactive_prize; // "166.0000", 优惠
+    public String wages; // "215.0000", 返水
+    public String fee; // 0, 场馆费
     public String settlement; // -16878.2826,
-    public String deposit; // "10215.0000",
-    public String withdraw; // "519.0000",
+    public String deposit; // "10215.0000", 充值
+    public String withdraw; // "519.0000", 提款
     public String fee2; // "0.0000",
     public String isfrozen; // "0",
     public String registertime; // "2022-03-01 10:27:21",
