e8490018e753749aadc281b5ed9e14a6c958e989
hqxc-seka <hqxc.seka@gmail.com>
2024-03-20 14:34:27 +0800

    安全中心 增加密保设定 Activity配置、路由设置

--- library-base/src/main/java/com/xtree/base/router/RouterActivityPath.java
+++ library-base/src/main/java/com/xtree/base/router/RouterActivityPath.java
@@ -44,6 +44,7 @@ public class RouterActivityPath {
         public static final String PAGER_WITHDRAW = MINE + "/withdraw";
         public static final String PAGER_CHOOSE_WITHDRAW = MINE + "/choose";
         public static final String PAGER_MY_WALLET_FLOW = MINE + "/flow";//活动流水
+        public static final String PAGER_ACCOUNT_SECURITY = MINE +"/security";//账户安全 密保相关
     }
 
     /**
