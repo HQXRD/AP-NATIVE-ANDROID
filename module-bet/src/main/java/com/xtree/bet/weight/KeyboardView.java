dc94460eee7889d574f5f0f2d75568a405305db9
hqxc-marquis <hqxc.marquis@gmail.com>
2024-03-26 09:51:57 +0800

    场馆 解决输入投注金额时偶现崩溃问题

--- module-bet/src/main/java/com/xtree/bet/weight/KeyboardView.java
+++ module-bet/src/main/java/com/xtree/bet/weight/KeyboardView.java
@@ -107,7 +107,7 @@ public class KeyboardView extends FrameLayout implements View.OnClickListener {
         rvNumber.setAdapter(new CommonAdapter<String>(context, R.layout.bt_layout_keyboard_item, Arrays.asList(numbers)) {
             @Override
             protected void convert(ViewHolder holder, String number, int position) {
-                if(editText != null && !editText.isEnabled()){
+                if(editText == null && !editText.isEnabled()){
                     return;
                 }
                 holder.setText(R.id.tv_item, number);
@@ -142,7 +142,7 @@ public class KeyboardView extends FrameLayout implements View.OnClickListener {
             }
         });
         findViewById(R.id.tv_max).setOnClickListener(view -> {
-            if(!editText.isEnabled()){
+            if(editText == null || !editText.isEnabled()){
                 return;
             }
             CharSequence hint = editText.getHint();
