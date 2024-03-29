1e59e5a98032782ed40c873090e8ce4256c11a28
hqxc-ace <hqxc.ace@gmail.com>
2024-03-29 16:36:06 +0800

    公共类 修改默认域名

--- library-base/src/main/java/com/xtree/base/utils/DomainUtil.java
+++ library-base/src/main/java/com/xtree/base/utils/DomainUtil.java
@@ -4,8 +4,8 @@ import android.text.TextUtils;
 
 public class DomainUtil {
 
-    private static String apiUrl = "https://app1.vcchgk.com"; // API 用
-    private static String domainUrl = "https://www.alltm1s.com"; // 网页用
+    private static String apiUrl = "https://www.eon2.org"; // API 用
+    private static String domainUrl = "https://www.eon2.org"; // 网页用
 
     /**
      * 获取域名, API 使用
