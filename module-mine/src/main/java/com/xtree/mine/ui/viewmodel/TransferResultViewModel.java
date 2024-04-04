b03d4667f68c3ddb671ea411596ed2bbc87ddd11
hqxc-kaka <hqxc.kaka@gmail.com>
2024-04-04 17:54:15 +0800

    BugFix XSP-345:我的-转账-转账失败缺少交互页面

--- module-mine/src/main/java/com/xtree/mine/ui/viewmodel/TransferResultViewModel.java
+++ module-mine/src/main/java/com/xtree/mine/ui/viewmodel/TransferResultViewModel.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
+import com.xtree.base.contract.GamePlatformEnum;
 import com.xtree.base.mvvm.model.ToolbarModel;
 import com.xtree.mine.R;
 import com.xtree.mine.data.MineRepository;
@@ -24,6 +25,7 @@ public class TransferResultViewModel extends BaseViewModel<MineRepository> imple
     public final MutableLiveData<Drawable> statusImgData = new MutableLiveData<android.graphics.drawable.Drawable>();
     public final MutableLiveData<String> statusTitleData = new MutableLiveData<>();
     public final MutableLiveData<String> statusSubTitleData = new MutableLiveData<>();
+    public final MutableLiveData<String> buttonTextData = new MutableLiveData<>();
 
     public TransferResultViewModel(@NonNull Application application) {
         super(application);
@@ -31,18 +33,23 @@ public class TransferResultViewModel extends BaseViewModel<MineRepository> imple
 
     public TransferResultViewModel(@NonNull Application application, MineRepository model) {
         super(application, model);
+
     }
 
     public void initData(TransferResultModel model) {
         statusData.setValue(model.status);
+        String fromName = GamePlatformEnum.checkCode(model.from).getName();;
+        String toName = GamePlatformEnum.checkCode(model.to).getName();;
         if (model.status == 1) {
             statusTitleData.setValue("转账成功");
+            buttonTextData.setValue("继续转账");
             statusImgData.setValue(getApplication().getDrawable(R.mipmap.icon_transfer_success));
-            statusSubTitleData.setValue("您已从" + model.from + "转入" + model.to + model.money + "元");
+            statusSubTitleData.setValue("您已从" + fromName + "转入" + toName + model.money + "元");
         } else {
             statusTitleData.setValue("转账失败");
+            buttonTextData.setValue("返回上一步");
             statusImgData.setValue(getApplication().getDrawable(R.mipmap.icon_transfer_fail));
-            statusSubTitleData.setValue("您从" + model.from + "转入" + model.to + model.money + "元转账失败" + "\n转账失败(" + model.errorMsg + ")");
+            statusSubTitleData.setValue("您从" + fromName + "转入" + toName + model.money + "元转账失败" + "\n" + model.errorMsg);
         }
     }
 
