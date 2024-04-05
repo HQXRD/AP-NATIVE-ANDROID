6acd129a0206657f11d56a88a080bce71a969aff
hqxc-mobius <hqxc.mobius@gmail.com>
2024-04-05 18:50:34 +0800

    我的 调整绑定数字钱包判断
    (cherry picked from commit 6909253600452e3104132d7a29011d24a2e9ca00)

--- module-mine/src/main/java/com/xtree/mine/vo/BindCountsVo.java
+++ module-mine/src/main/java/com/xtree/mine/vo/BindCountsVo.java
@@ -1,6 +1,6 @@
 package com.xtree.mine.vo;
 
 public class BindCountsVo {
-    public String bank_count; // "1"
-    public String digit_count; // "1"
+    public int bank_count; // "1"
+    public int digit_count; // "1"
 }
