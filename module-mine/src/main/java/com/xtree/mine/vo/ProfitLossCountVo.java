b28665355a1e05a1ef835fa999fad2675524f8a4
hqxc-mobius <hqxc.mobius@gmail.com>
2024-04-08 18:10:58 +0800

    我的 盈亏报表回传值为空
    (cherry picked from commit ed591de46792350bee54cd3a1229e19f6f4cc135)

--- module-mine/src/main/java/com/xtree/mine/vo/ProfitLossCountVo.java
+++ module-mine/src/main/java/com/xtree/mine/vo/ProfitLossCountVo.java
@@ -5,17 +5,17 @@ package com.xtree.mine.vo;
  */
 public class ProfitLossCountVo {
 
-    public String sbet; // "0.0000",
-    public String spoint; // "0.0000",
-    public String seffective_bet; // "0.0000",
-    public String liushui; // "0.0000",
-    public String sprize; // "0.0000",
-    public String sprofit_loss; // "0.0000",
-    public String sactive_prize; // "0.0000",
-    public String wages; // "0.0000",
-    public String fee; // "0.0000",
-    public String settlement; // "0.0000", 90
-    public String deposit; // "0.0000",
-    public String withdraw; // "0.0000"
+    public String sbet = "0"; // "0.0000",
+    public String spoint = "0"; // "0.0000",
+    public String seffective_bet = "0"; // "0.0000",
+    public String liushui = "0"; // "0.0000",
+    public String sprize = "0.0"; // "0.0000",
+    public String sprofit_loss = "0.0"; // "0.0000",
+    public String sactive_prize = "0"; // "0.0000",
+    public String wages = "0"; // "0.0000",
+    public String fee = "0"; // "0.0000",
+    public String settlement = "0.0"; // "0.0000", 90
+    public String deposit = "0"; // "0.0000",
+    public String withdraw = "0"; // "0.0000"
 
 }
