c548e80fa923e1099ab24964cb56489dea3d99b4
hqxc-seka <hqxc.seka@gmail.com>
2024-04-08 12:17:01 +0800

    提款 提款模块增加有效流水限制

--- module-mine/src/main/java/com/xtree/mine/ui/fragment/AwardsRecordDialog.java
+++ module-mine/src/main/java/com/xtree/mine/ui/fragment/AwardsRecordDialog.java
@@ -99,7 +99,22 @@ public class AwardsRecordDialog extends BottomPopupView {
             tipText = "您今日没有可用提款次数";
         }
         if (awardsRecordVo !=null){
-            if (awardsRecordVo.list!=null &&  !awardsRecordVo.list.isEmpty()) {
+            if (!TextUtils.isEmpty(awardsRecordVo.withdraw_dispensing_money) &&!TextUtils.isEmpty(awardsRecordVo.locked_award_sum)  &&awardsRecordVo.list!=null &&  !awardsRecordVo.list.isEmpty()){
+                tipText  = String.format(getContext().getString(R.string.txt_awards_flow_title) , awardsRecordVo.withdraw_dispensing_money) ;
+                binding.tvChooseTip.setText(tipText);
+                binding.llChooseTip.setVisibility(View.VISIBLE);
+                binding.lvChoose.setVisibility(View.VISIBLE);
+                ChooseAdapter adapter = new ChooseAdapter(getContext(), awardsRecordVo.list);
+                binding.lvChoose.setAdapter(adapter);
+
+            } else if (TextUtils.equals("0.00" , awardsRecordVo.locked_award_sum) && !TextUtils.isEmpty(awardsRecordVo.withdraw_dispensing_money)) {
+                tipText  = String.format(getContext().getString(R.string.txt_awards_flow_title) , awardsRecordVo.withdraw_dispensing_money) ;
+                binding.tvChooseTip.setText(tipText);
+                binding.llChooseTip.setVisibility(View.VISIBLE);
+                binding.lvChoose.setVisibility(View.GONE);
+                binding.tvWithdrawalAwardsTitle.setVisibility(View.GONE);
+
+            } else if (awardsRecordVo.list!=null &&  !awardsRecordVo.list.isEmpty()) {
                 binding.tvChooseTip.setVisibility(View.GONE);
                 binding.llChooseTip.setVisibility(View.GONE);
                 binding.lvChoose.setVisibility(View.VISIBLE);
