d9b0215a85e12f0d3d2b2e221973962913e1a3e9
hqxc-daniel <hqxc.daniel@gmail.com>
2024-04-15 12:32:25 +0800

    三方游戏 AG/DB真人场馆增加弹窗提示

--- library-base/src/main/java/com/xtree/base/global/SPKeyGlobal.java
+++ library-base/src/main/java/com/xtree/base/global/SPKeyGlobal.java
@@ -27,6 +27,8 @@ public class SPKeyGlobal {
     public static final String RC_NOT_TIP_TODAY_COUNT = "rc_not_tip_today_count"; // 充值 今日不再提示 次数
     public static final String RC_NOT_TIP_TODAY_LOW = "rc_not_tip_today_low"; // 充值 今日不再提示 低成功率
     public static final String PM_NOT_TIP_TODAY = "pm_not_tip_today"; // 熊猫体育 今日不再提示
+    public static final String AG_NOT_TIP_TODAY = "ag_not_tip_today"; // AG真人 今日不再提示
+    public static final String DB_NOT_TIP_TODAY = "db_not_tip_today"; // DB真人 今日不再提示
     public static final String RC_PAYMENT_THIRIFRAME = "rc_payment_thiriframe";
     public static final String WLT_CENTRAL_BLC = "wlt_central_blc";
     public static final String WLT_GAME_ROOM_BLC = "wlt_game_room_blc";
---
+++ library-base/src/main/java/com/xtree/base/widget/TipGameDialog.java
@@ -0,0 +1,58 @@
+package com.xtree.base.widget;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.lxj.xpopup.core.CenterPopupView;
+import com.xtree.base.R;
+import com.xtree.base.databinding.DialogGameTipBinding;
+import com.xtree.base.utils.TimeUtils;
+
+import me.xtree.mvvmhabit.utils.KLog;
+import me.xtree.mvvmhabit.utils.SPUtils;
+
+public class TipGameDialog extends CenterPopupView {
+
+    private String title;
+    private String key;
+    DialogGameTipBinding binding;
+
+    public TipGameDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public TipGameDialog(@NonNull Context context, String title, String key) {
+        super(context);
+        this.title = title;
+        this.key = key;
+    }
+
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        initView();
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.dialog_game_tip;
+    }
+
+    private void initView() {
+        binding = DialogGameTipBinding.bind(findViewById(R.id.ll_root));
+        binding.tvwConfirm.setOnClickListener(v -> dismiss());
+        String msg = title + getContext().getString(R.string.text_cancel_tip);
+        binding.tvwMsg.setText(msg);
+        binding.cbTipPm.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            KLog.i("isChecked", isChecked);
+            if (isChecked) {
+                SPUtils.getInstance().put(key, TimeUtils.getCurDate());
+            } else {
+                SPUtils.getInstance().remove(key);
+            }
+        });
+
+    }
+
+}
