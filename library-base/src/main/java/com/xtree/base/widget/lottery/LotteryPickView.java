3e38566294752a36eeede60b46c1605865a00f3e
hqxc-kaka <hqxc.kaka@gmail.com>
2024-04-25 10:48:54 +0800

    彩种投注 复式冷热遗漏功能封装

--- library-base/src/main/java/com/xtree/base/widget/lottery/LotteryPickView.java
+++ library-base/src/main/java/com/xtree/base/widget/lottery/LotteryPickView.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.xtree.base.databinding.LayoutLotteryPickBinding;
+
+import java.util.List;
 
 /**
  * Created by KAKA on 2024/4/22.
@@ -15,25 +18,136 @@ import com.xtree.base.databinding.LayoutLotteryPickBinding;
  */
 public class LotteryPickView extends LinearLayout {
 
+    private LotteryPickViewModel lotteryPickViewModel = null;
+
+    public interface onPickListener{
+        void onPick(List<Integer> pickNums);
+    }
+
     private LayoutLotteryPickBinding binding;
 
     public LotteryPickView(Context context) {
         super(context);
-        binding = LayoutLotteryPickBinding.inflate(LayoutInflater.from(context), this, false);
+        binding = LayoutLotteryPickBinding.inflate(LayoutInflater.from(context), this, true);
 
         initView();
     }
 
     public LotteryPickView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        binding = LayoutLotteryPickBinding.inflate(LayoutInflater.from(context), this, false);
+        binding = LayoutLotteryPickBinding.inflate(LayoutInflater.from(context), this, true);
 
         initView();
     }
 
-    private void initView() {
-
-
+    /**
+     * 设置号码选中监听
+     */
+    public void setPickListener(onPickListener pickListener) {
+        if (lotteryPickViewModel != null) {
+            lotteryPickViewModel.pickListener = pickListener;
+        }
     }
 
+    private void initView() {
+        lotteryPickViewModel = new LotteryPickViewModel();
+        lotteryPickViewModel.layoutManager.set(new GridLayoutManager(getContext(), 5));
+        lotteryPickViewModel.initData(null);
+        binding.setModel(lotteryPickViewModel);
+    }
+
+    public LotteryPickView setCustomData(List<String> list) {
+        lotteryPickViewModel.initData(list);
+        return this;
+    }
+
+    public LotteryPickView toSingleMode() {
+        lotteryPickViewModel.setMode(LotteryPickViewModel.SINGLE_MODE);
+        return this;
+    }
+
+    /**
+     * 显示遗漏
+     */
+    public void showOmission(List<Integer> list) {
+        lotteryPickViewModel.showOmission(list);
+    }
+
+    /**
+     * 显示冷热
+     */
+    public void showHotCold(List<Integer> list) {
+        lotteryPickViewModel.showHotCold(list);
+    }
+
+    /**
+     * 关闭遗漏、冷热
+     */
+    public void closeTag() {
+        lotteryPickViewModel.closeTag();
+    }
+
+    /**
+     * 获取所有选中号码
+     */
+    public List<Integer> getPick() {
+        return lotteryPickViewModel.getPick();
+    }
+
+    /**
+     * 选中所有
+     */
+    public void pickAll() {
+        if (lotteryPickViewModel.getMode() == LotteryPickViewModel.SINGLE_MODE) {
+            return;
+        }
+        lotteryPickViewModel.pickAll();
+    }
+
+    /**
+     * 选中大数
+     */
+    public void pickLarge() {
+        if (lotteryPickViewModel.getMode() == LotteryPickViewModel.SINGLE_MODE) {
+            return;
+        }
+        lotteryPickViewModel.pickLarge();
+    }
+
+    /**
+     * 选中小数
+     */
+    public void pickSmall() {
+        if (lotteryPickViewModel.getMode() == LotteryPickViewModel.SINGLE_MODE) {
+            return;
+        }
+        lotteryPickViewModel.pickSmall();
+    }
+
+    /**
+     * 选中奇数
+     */
+    public void pickOdd() {
+        if (lotteryPickViewModel.getMode() == LotteryPickViewModel.SINGLE_MODE) {
+            return;
+        }
+        lotteryPickViewModel.pickOdd();
+    }
+
+    /**
+     * 选中偶数
+     */
+    public void pickEven() {
+        if (lotteryPickViewModel.getMode() == LotteryPickViewModel.SINGLE_MODE) {
+            return;
+        }
+        lotteryPickViewModel.pickEven();
+    }
+
+    /**
+     * 清除所有
+     */
+    public void pickClear() {
+        lotteryPickViewModel.pickClear();
+    }
 }
--- module-mine/src/main/java/com/xtree/mine/ui/rebateagrt/fragment/RebateAgreementFragment.java
+++ module-mine/src/main/java/com/xtree/mine/ui/rebateagrt/fragment/RebateAgreementFragment.java
@@ -9,11 +9,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.xtree.base.router.RouterFragmentPath;
+import com.xtree.base.widget.lottery.LotteryPickView;
 import com.xtree.mine.BR;
 import com.xtree.mine.R;
 import com.xtree.mine.databinding.FragmentRebateAgreementBinding;
 import com.xtree.mine.ui.rebateagrt.viewmodel.RebateAgreementViewModel;
 import com.xtree.mine.ui.viewmodel.factory.AppViewModelFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.xtree.mvvmhabit.base.BaseFragment;
 
@@ -48,5 +52,39 @@ public class RebateAgreementFragment extends BaseFragment<FragmentRebateAgreemen
         super.initData();
         viewModel.setActivity(getActivity());
         viewModel.initData();
+
+        binding.lotteryPickView.setPickListener(new LotteryPickView.onPickListener() {
+            @Override
+            public void onPick(List<Integer> pickNums) {
+                pickNums.toString();
+            }
+        });
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("龍");
+        strings.add("虎");
+        strings.add("莊");
+        strings.add("閑");
+
+        ArrayList<Integer> olist = new ArrayList<>();
+        olist.add(12);
+        olist.add(3);
+        olist.add(44);
+        olist.add(555);
+        olist.add(223);
+        olist.add(4);
+        olist.add(4);
+        olist.add(5);
+        olist.add(23);
+        olist.add(68);
+        binding.lotteryPickView
+                .toSingleMode();
+
+        binding.bt1.setOnClickListener(v -> binding.lotteryPickView.showOmission(olist));
+        binding.bt2.setOnClickListener(v -> binding.lotteryPickView.closeTag());
+        binding.bt3.setOnClickListener(v -> binding.lotteryPickView.showHotCold(olist));
+        binding.bt4.setOnClickListener(v -> binding.lotteryPickView.pickOdd());
+        binding.bt5.setOnClickListener(v -> binding.lotteryPickView.pickEven());
+        binding.bt6.setOnClickListener(v -> binding.lotteryPickView.pickClear());
     }
 }
