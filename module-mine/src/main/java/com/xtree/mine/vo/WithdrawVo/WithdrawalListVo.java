d0b681da3d6e629f24088d1b802e99afce48f76a
hqxc-seka <hqxc.seka@gmail.com>
2024-04-26 09:09:43 +0800

    提款 提款优化【奥丁】

---
+++ module-mine/src/main/java/com/xtree/mine/vo/WithdrawVo/WithdrawalListVo.java
@@ -0,0 +1,62 @@
+package com.xtree.mine.vo.WithdrawVo;
+
+import me.xtree.mvvmhabit.http.BaseResponse2;
+
+/**
+ * 选择提款模式
+ */
+public class WithdrawalListVo extends BaseResponse2 {
+    public String name ;
+    public String title;
+    public String type;
+    public String enable;
+    /*{
+	"status": 10000,
+	"message": "success",
+	"data": [
+		{
+			"name": "usdt",
+			"title": "USDT提款",
+			"type": "2",
+			"enable": true
+		},
+		{
+			"name": "hipayht",
+			"title": "固额提现",
+			"type": "1",
+			"enable": true
+		},
+		{
+			"name": "onepayfast3",
+			"title": "通用提现",
+			"type": "1",
+			"enable": true
+		},
+		{
+			"name": "hipaytx",
+			"title": "天下提现",
+			"type": "1",
+			"enable": true
+		},
+		{
+			"name": "generalchannel",
+			"title": "大额提现",
+			"type": "1",
+			"enable": true
+		},
+		{
+			"name": "ebpay",
+			"title": "EBPAY提款",
+			"type": "4",
+			"enable": true
+		},
+		{
+			"name": "hiwallet",
+			"title": "CNYT提款",
+			"type": "6",
+			"enable": true
+		}
+	],
+	"timestamp": 1714027704
+}*/
+}
