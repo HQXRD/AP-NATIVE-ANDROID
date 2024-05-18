57b1e6f3968e964ab7c9dea3eacffc68a0c5ac9b
hqxc-seka <hqxc.seka@gmail.com>
2024-05-18 17:36:02 +0800

    我的 安全中航信 修改密码（单号：HQAP2-3720）

--- module-mine/src/main/java/com/xtree/mine/ui/fragment/ChangePwdFragment.java
+++ module-mine/src/main/java/com/xtree/mine/ui/fragment/ChangePwdFragment.java
@@ -2,7 +2,9 @@ package com.xtree.mine.ui.fragment;
 
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
@@ -54,20 +56,31 @@ public class ChangePwdFragment extends BaseFragment<FragmentChangePwdBinding, Ve
         binding.ivwBack.setOnClickListener( v ->{
             getActivity().finish();
         });
+
         binding.tvwOk.setOnClickListener(v -> {
             String pwd1 = binding.edtPwd.getText().toString();
             String pwd2 = binding.edtPwd2.getText().toString();
 
-            if (pwd1.isEmpty()) {
-                ToastUtils.showLong(R.string.txt_pwd_cannot_empty);
+            if (pwd1.isEmpty() || pwd2.isEmpty())
+            {
+                showError(getString(R.string.txt_chang_psw_input_error));
                 return;
             }
             if (!pwd1.equals(pwd2)) {
-                ToastUtils.showLong(R.string.txt_pwd_not_same);
+                showError(getString(R.string.txt_chang_psw_input_twice_error));
+                return;
+            }
+            if (pwd1.length() < 6 || pwd1.length() > 16 ||pwd2.length() < 6 || pwd2.length() > 16  ){
+                showError(getString(R.string.txt_chang_psw_input_length_error));
+                return;
+            }
+            if (!pwd1.matches("^[0-9a-zA-Z]+$") || !pwd2.matches("^[0-9a-zA-Z]+$")){
+                showError(getString(R.string.txt_chang_psw_input_format_error));
                 return;
             }
 
-            String public_key = SPUtils.getInstance().getString("public_key", "");
+            String public_key = SPUtils.getInstance().getString("public_key",
+                    "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDW+Gv8Xmk+EdTLQUU5fEAzhlVuFrI7GN4a8N\\/B0Oe63ORK8oBE1pK+t5U5Iz89K4zf7nX+tqQvzND5Z57NMwyqTYYb3TMbrKgjqF1K2YW08OaubjpdohMnDIibmPXNtrbRZpOf2xIaApR+wpqGS+Xw0LzKA8JPYDOPO4lseAtqVwIDAQAB");
             String loginpass = RSAEncrypt.encrypt2(pwd1, public_key);
 
             HashMap<String, String> map = new HashMap<>();
@@ -125,4 +138,13 @@ public class ChangePwdFragment extends BaseFragment<FragmentChangePwdBinding, Ve
         edt.setSelection(edt.length());
     }
 
+
+    private void  showError(final String message){
+        if (TextUtils.isEmpty(message) ||  message == null){
+            return;
+        }
+        binding.tvwPswWarning.setVisibility(View.VISIBLE);
+        binding.tvwPswWarning.setText(message);
+
+    }
 }
