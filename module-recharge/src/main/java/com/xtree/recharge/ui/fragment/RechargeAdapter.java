cf6dc646e9cf5efa4980942c1989cd770069ecc4
hqxc-daniel <hqxc.daniel@gmail.com>
2024-05-27 20:11:37 +0800

    充值 获取图片逻辑修改

--- module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeAdapter.java
+++ module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeAdapter.java
@@ -2,12 +2,15 @@ package com.xtree.recharge.ui.fragment;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.xtree.base.adapter.CacheViewHolder;
 import com.xtree.base.adapter.CachedAutoRefreshAdapter;
@@ -16,8 +19,31 @@ import com.xtree.recharge.R;
 import com.xtree.recharge.databinding.ItemRechargeBinding;
 import com.xtree.recharge.vo.RechargeVo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RechargeAdapter extends CachedAutoRefreshAdapter<RechargeVo> {
 
+    public List<String> bankCodeList = Arrays.asList("abc", "bcm", "ccb", "cmb", "cmb2", "icbc");
+    public List<String> jsCode = Arrays.asList("onepayfix", "onepayfix2", "onepayfix3");
+    public List<String> szrmb = Arrays.asList("ecny", "ecnyhqppay", "jxpayecny1", "jxpayecny2", "jxpayecny3");
+    public List<String> ucybf = Arrays.asList("cryptohqppay2", "cryptotrchqppay2");
+    public static final List<String> hqppaybank = Arrays.asList(
+            "hqppaybank1", "hqppaybank1_sm", "hqppaybank2", "hqppaybank2_sm",
+            "hqppaybank3", "hqppayunionpayscan", "hqppayunionpayscan1",
+            "hqppayunionpayscan2", "hqppayunionpayscan2_sm"
+    );
+    public static final List<String> hqppayJx = Arrays.asList(
+            "hqppay", "hqppay1", "hqppay2", "hqppay3", "hqppay4",
+            "juxinpay", "juxinpay1", "juxinpay2", "jxpayjd", "jxpayjd1",
+            "jxpaykj", "jxpaykj1", "jxpaytz", "jxpayyl", "jxpayyl1"
+    );
+    public static final List<String> hqppay6 = Arrays.asList("hqppay6"); // UC天下支付
+    public static final List<String> onepay = Arrays.asList(
+            "onepay", "onepay1", "onepay2", "onepay3", "onepay4",
+            "onepayfix4", "onepayfix5", "onepay5", "onepay6",
+            "onepay7", "onepay8"
+    );
     Context ctx;
 
     ICallBack mCallBack;
@@ -71,9 +97,10 @@ public class RechargeAdapter extends CachedAutoRefreshAdapter<RechargeVo> {
         int visible2 = vo.recommend == 0 ? View.INVISIBLE : View.VISIBLE;
         binding.ivwRcmd.setVisibility(visible2); // 推荐
         //binding.tvwTitle.setCompoundDrawables();
-        Drawable dr = ctx.getDrawable(R.drawable.rc_ic_pmt_selector);
-        dr.setLevel(Integer.parseInt(vo.bid));
-        binding.tvwTitle.setCompoundDrawablesRelativeWithIntrinsicBounds(null, dr, null, null);
+        //Drawable dr = ctx.getDrawable(R.drawable.rc_ic_pmt_selector);
+        //dr.setLevel(Integer.parseInt(vo.bid));
+        getImageLevel(vo, binding.tvwTitle);
+        //binding.tvwTitle.setCompoundDrawablesRelativeWithIntrinsicBounds(null, dr, null, null);
         //holder.itemView.setOnClickListener(v -> binding.tvwTitle.performClick());
         binding.tvwTitle.setOnClickListener(v -> {
 
@@ -86,6 +113,88 @@ public class RechargeAdapter extends CachedAutoRefreshAdapter<RechargeVo> {
             curBid = vo.bid;
             mCallBack.onClick(vo);
         });
+    }
+
+    private void getImageLevel(RechargeVo bank, TextView tvwTitle) {
+        switch (bank.paycode) {
+            case "hqppaytopay":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_topay, R.mipmap.rc_ic_pmt_topay_selected, tvwTitle);
+                break;
+            case "hqppayokpay":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_okpay, R.mipmap.rc_ic_pmt_okpay_selected, tvwTitle);
+                break;
+            case "hqppaygopay":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_gopay, R.mipmap.rc_ic_pmt_gopay_selected, tvwTitle);
+                break;
+            case "ebpay":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_ebkc, R.mipmap.rc_ic_pmt_ebkc_selected, tvwTitle);
+                break;
+            case "abc":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_abc, R.mipmap.rc_ic_pmt_abc_selected, tvwTitle);
+                break;
+            case "bcm":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_bcm, R.mipmap.rc_ic_pmt_bcm_selected, tvwTitle);
+                break;
+            case "ccb":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_ccb, R.mipmap.rc_ic_pmt_ccb_selected, tvwTitle);
+                break;
+            case "cmb":
+            case "cmb2":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_cmb, R.mipmap.rc_ic_pmt_cmb_selected, tvwTitle);
+                break;
+            case "icbc":
+                addSelectorFromDrawable(R.mipmap.rc_ic_pmt_icbc, R.mipmap.rc_ic_pmt_icbc_selected, tvwTitle);
+                break;
+            default:
+                if (jsCode.contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_jisuchongzhi, R.mipmap.rc_ic_pmt_jisuchongzhi_selected, tvwTitle);
+                } else if (szrmb.contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_szrmb, R.mipmap.rc_ic_pmt_szrmb_selected, tvwTitle);
+                } else if ((bank.title.contains("USDT") || bank.title.contains("TRC") || bank.title.contains("ERC")) && !ucybf.contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_usdt, R.mipmap.rc_ic_pmt_usdt_selected, tvwTitle);
+                } else if (bank.title.contains("微信")) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_wx, R.mipmap.rc_ic_pmt_wx_selected, tvwTitle);
+                } else if (bank.title.contains("支付宝")) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_zfb, R.mipmap.rc_ic_pmt_zfb_selected, tvwTitle);
+                } else if (bank.title.contains("QQ")) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_qq, R.mipmap.rc_ic_pmt_qq_selected, tvwTitle);
+                } else if (bank.title.contains("客服代充")) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_happy, R.mipmap.rc_ic_pmt_happy_selected, tvwTitle);
+                } else if ("hiwallet".contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_cn, R.mipmap.rc_ic_pmt_cn_selected, tvwTitle);
+                } else if (ucybf.contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_ucybf, R.mipmap.rc_ic_pmt_ucybf_selected, tvwTitle);
+                }
+                //} else if (hqppaybank.contains(bank.paycode)) {
+                //    model = new XCDepositBankIcon(R.drawable.xc_hqppaybank, R.drawable.xc_hqppaybank_active);
+                //} else if (hqppayJx.contains(bank.paycode)) {
+                //    model = new XCDepositBankIcon(R.drawable.xc_hqppayJx, R.drawable.xc_hqppayJx_active);
+                //} else if (hqppay6.contains(bank.paycode)) {
+                //    model = new XCDepositBankIcon(R.drawable.xc_hqppay6, R.drawable.xc_hqppay6_active);
+                //}
+                else if (onepay.contains(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_yhk, R.mipmap.rc_ic_pmt_yhk_selected, tvwTitle);
+                } else if (("hqppaympay").equals(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_mpay, R.mipmap.rc_ic_pmt_mpay_selected, tvwTitle);
+                } else if (("hqppaygobao").equals(bank.paycode)) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_gobao, R.mipmap.rc_ic_pmt_gobao_selected, tvwTitle);
+                } else if (bank.paycode.equals("manual")) {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_manual, R.mipmap.rc_ic_pmt_manual_selected, tvwTitle);
+                } else {
+                    addSelectorFromDrawable(R.mipmap.rc_ic_pmt_happy, R.mipmap.rc_ic_pmt_happy_selected, tvwTitle);
+                }
+                break;
+        }
+
+    }
+
+    public void addSelectorFromDrawable(int idNormal, int idPress, TextView tv) {
+        StateListDrawable drawable = new StateListDrawable();
+        Drawable normal = ContextCompat.getDrawable(ctx, idNormal);
+        Drawable press = ContextCompat.getDrawable(ctx, idPress);
+        drawable.addState(new int[]{android.R.attr.state_pressed}, press);
+        drawable.addState(new int[]{-android.R.attr.state_pressed}, normal);
+        tv.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable, null, null);
     }
 
 }
