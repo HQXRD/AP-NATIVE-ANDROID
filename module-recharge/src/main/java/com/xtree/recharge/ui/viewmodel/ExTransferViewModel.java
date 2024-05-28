9f5a22329eb39eca1d4b000637a30198b291f099
hqxc-kaka <hqxc.kaka@gmail.com>
2024-05-28 20:22:06 +0800

    充值 增加电子回单示例

--- module-recharge/src/main/java/com/xtree/recharge/ui/viewmodel/ExTransferViewModel.java
+++ module-recharge/src/main/java/com/xtree/recharge/ui/viewmodel/ExTransferViewModel.java
@@ -1,14 +1,31 @@
 package com.xtree.recharge.ui.viewmodel;
 
 import android.app.Application;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.MutableLiveData;
 
+import com.luck.picture.lib.basic.PictureSelector;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.config.SelectMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.interfaces.OnResultCallbackListener;
+import com.xtree.base.utils.CfLog;
+import com.xtree.base.widget.GlideEngine;
+import com.xtree.base.widget.ImageFileCompressEngine;
 import com.xtree.recharge.data.RechargeRepository;
+import com.xtree.recharge.ui.fragment.ExSlipExampleDialogFragment;
 
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Flowable;
@@ -30,6 +47,8 @@ public class ExTransferViewModel extends BaseViewModel<RechargeRepository> {
 
     public MutableLiveData<String> paymentTime = new MutableLiveData<>();
     public MutableLiveData<String> waitTime = new MutableLiveData<>();
+    //凭证图片
+    public MutableLiveData<Uri> voucher = new MutableLiveData<>();
 
     private WeakReference<FragmentActivity> mActivity = null;
 
@@ -83,7 +102,64 @@ public class ExTransferViewModel extends BaseViewModel<RechargeRepository> {
      * 回执单示例
      */
     public void showExample() {
+        ExSlipExampleDialogFragment.show(mActivity.get());
+    }
 
+    /**
+     * 图片选择
+     */
+    public void gotoSelectMedia() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent getpermission = new Intent();
+                getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                mActivity.get().startActivity(getpermission);
+                return;
+            }
+        }
+        PictureSelector.create(mActivity.get())
+                .openGallery(SelectMimeType.ofImage())
+                .isDisplayCamera(false)
+                .setMaxSelectNum(1)
+                .setImageEngine(GlideEngine.createGlideEngine())
+                .setCompressEngine(ImageFileCompressEngine.create())
+                .forResult(new OnResultCallbackListener<LocalMedia>() {
+                    @Override
+                    public void onResult(ArrayList<LocalMedia> result) {
+                        if (result != null) {
+                            for (int i = 0; i < result.size(); i++) {
+                                String imageRealPathString = result.get(i).getCompressPath();
+                                if (TextUtils.isEmpty(imageRealPathString)) {
+                                    imageRealPathString = result.get(i).getRealPath();
+                                }
+
+                                if (PictureMimeType.isContent(imageRealPathString)) {
+                                    voucher.setValue(Uri.parse(imageRealPathString));
+                                } else {
+                                    voucher.setValue(Uri.fromFile(new File(imageRealPathString)));
+                                }
+                                CfLog.i("获取图片地址是 uri ====== " + voucher.getValue());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+    }
+
+    /**
+     * 上传图片
+     */
+    private void uploadImage(String imageFile) {
+//        HashMap<String, String> uploadMap = new HashMap<String, String>();
+//        uploadMap.put("type", "m4");
+//        uploadMap.put("nonce", UuidUtil.getID16());//传入UUID);
+//        uploadMap.put("filetype", "image/png");
+//        uploadMap.put("filedata", ImageUploadUtil.bitmapToString(imageFile));
+//        viewModel.feedbackImageUp(uploadMap);
     }
 
 }
