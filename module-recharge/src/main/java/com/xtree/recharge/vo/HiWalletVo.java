befd80988a5640b3c39948dd7145d448a38ed1ca
hqxc-ace <hqxc.ace@gmail.com>
2024-06-05 11:15:14 +0800

    充值 CNYT充值方式默认隐藏嗨钱包

--- module-recharge/src/main/java/com/xtree/recharge/vo/HiWalletVo.java
+++ module-recharge/src/main/java/com/xtree/recharge/vo/HiWalletVo.java
@@ -3,7 +3,7 @@ package com.xtree.recharge.vo;
 public class HiWalletVo {
 
     public String login_url; // "https://h5.hicnyt-pre.com/signup?qy=1&platformCode=AS&platformUserId=5228471&userEmail="
-    public boolean is_registered; // false
+    public boolean is_registered = true; // 新加字段; false:未注册,要弹窗; true:已注册,不用弹窗,进入下一步; (默认true不弹窗)
     public String download_url; // "https://www.oeikdj.com/"
 
     @Override
