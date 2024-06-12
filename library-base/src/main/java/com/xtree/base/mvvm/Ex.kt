e2f4a9a230ea3a724cf465e9bd6db0cd2443e425
hqxc-kaka <hqxc.kaka@gmail.com>
2024-06-12 15:25:15 +0800

    充值 极速充值新增客服按钮&基本配置;扩大上传凭证触及范围

--- library-base/src/main/java/com/xtree/base/mvvm/Ex.kt
+++ library-base/src/main/java/com/xtree/base/mvvm/Ex.kt
@@ -1,5 +1,6 @@
 package com.xtree.base.mvvm
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Build
@@ -140,6 +141,7 @@ fun EditText.init(textChangedListener: TextWatcher?) {
     textChangedListener?.let { addTextChangedListener(it) }
 }
 
+@SuppressLint("CheckResult")
 @BindingAdapter(
     value = ["imageUrl", "placeholder", "error", "fallback", "loadWidth", "loadHeight", "cacheEnable"],
     requireAll = false
@@ -161,14 +163,13 @@ fun setImageUrl(
     val diskCacheStrategy = if (cacheEnable == true) DiskCacheStrategy.AUTOMATIC else DiskCacheStrategy.NONE
     // 设置编码格式，在Android 11(R)上面使用高清无损压缩格式 WEBP_LOSSLESS ， Android 11 以下使用PNG格式，PNG格式时会忽略设置的 quality 参数。
     val encodeFormat = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) Bitmap.CompressFormat.WEBP_LOSSLESS else Bitmap.CompressFormat.PNG
-    Glide.with(view.context)
+    val glide = Glide.with(view.context)
         .asDrawable()
         .load(source)
         .placeholder(placeholder)
         .error(error)
         .fallback(fallback)
         .thumbnail(0.33f)
-        .override(widthSize, heightSize)
         .skipMemoryCache(false)
         .sizeMultiplier(0.5f)
         .format(DecodeFormat.PREFER_ARGB_8888)
@@ -176,5 +177,10 @@ fun setImageUrl(
         .encodeQuality(80)
         .diskCacheStrategy(diskCacheStrategy)
         .transition(DrawableTransitionOptions.withCrossFade())
-        .into(view)
+    width?.let {
+        if (it > 0) {
+            glide.override(widthSize, heightSize)
+        }
+    }
+    glide.into(view)
 }
