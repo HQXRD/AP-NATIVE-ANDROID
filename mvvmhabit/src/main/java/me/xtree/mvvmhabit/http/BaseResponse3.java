29e577cb8445b905e5c7632083a24b5e7d658322
hqxc-seka <hqxc.seka@gmail.com>
2024-06-18 14:55:19 +0800

    提款 网络层使用专用网络层

---
+++ mvvmhabit/src/main/java/me/xtree/mvvmhabit/http/BaseResponse3.java
@@ -0,0 +1,93 @@
+package me.xtree.mvvmhabit.http;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * 该类仅供参考，实际业务返回的固定字段, 根据需求来定义
+ */
+public class BaseResponse3<T> {
+    private int status = -1;
+    private int code = -1;
+    private boolean success;
+    @SerializedName(value = "message", alternate = {"msg", "sMsg"})
+    @Expose
+    private String message = ""; // msg, sMsg
+    private AuthVo authorization;
+    public int timestamp; // 1700702751
+    private T data;
+
+    public class AuthVo {
+        public String token; // "eyJ0eXAiOi***NTViMg"
+        public String token_type; // "bearer"
+        public int expires_in; // 604800 (7 day)
+
+        @Override
+        public String toString() {
+            return "AuthVo{" +
+                    "token='" + token + '\'' +
+                    ", token_type='" + token_type + '\'' +
+                    ", expires_in=" + expires_in +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BaseResponse{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                ", timestamp=" + timestamp +
+                ", data=" + data +
+                '}';
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isOk() {
+        return status == 10000;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public AuthVo getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(AuthVo authorization) {
+        this.authorization = authorization;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
