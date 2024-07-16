dd439641a459ac8429fae752c766dfd008fc2f6d
hqxc-seka <hqxc.seka@gmail.com>
2024-07-17 01:05:57 +0800

    【安卓】注册验证码优化(4513)[补充注册页面增加settingVo非空判断]

--- module-mine/src/main/java/com/xtree/mine/ui/activity/LoginRegisterActivity.java
+++ module-mine/src/main/java/com/xtree/mine/ui/activity/LoginRegisterActivity.java
@@ -554,7 +554,9 @@ public class LoginRegisterActivity extends BaseActivity<ActivityLoginBinding, Lo
                 mIsPwd2 = false;
             }
             //注册页面需要判断Seting 状态 1需要判断
-            if (TextUtils.equals("1", settingsVo.register_captcha_switch)) {
+            if (settingsVo !=null &&
+                    settingsVo.register_captcha_switch != null
+                    &&TextUtils.equals("1", settingsVo.register_captcha_switch)) {
                 //验证码不能为空
                 if (verificationTxt.isEmpty()) {
                     binding.tvwPwdCheckVerification.setVisibility(View.VISIBLE);
