c171d2198980dcfd14a868578d0f2170ee379179
hqxc-seka <hqxc.seka@gmail.com>
2024-07-31 09:57:34 +0800

    点更新后提示-解析软件包时出现问题（641）

--- module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
+++ module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
@@ -241,29 +241,36 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         viewModel.liveDataUpdate.observe(getViewLifecycleOwner(), vo -> {
             updateVo = vo;
             if (updateVo != null) {
-                //存储服务器设置时间间隔
-                SPUtils.getInstance().put(SPKeyGlobal.APP_INTERVAL_TIME, updateVo.interval_duration);
-                //请求更新服务时间
-                SPUtils.getInstance().put(SPKeyGlobal.APP_LAST_CHECK_TIME, System.currentTimeMillis());
-                long versionCode = Long.valueOf(StringUtils.getVersionCode(getContext()));
-                CfLog.i("versionCode = " + versionCode);
-                if (versionCode < updateVo.version_code) {
-                    //线上版本大于本机版本
-                    if (updateVo.type == 0) {
-                        //弱更
-                        if (versionCode >= vo.version_code_min) {
-                            showUpdate(true, updateVo); // 弱更
-                        } else {
-                            showUpdate(false, updateVo); // 强更
-                        }
+                if (updateVo.download_url.contains(".apk"))
+                {
+                    //存储服务器设置时间间隔
+                    SPUtils.getInstance().put(SPKeyGlobal.APP_INTERVAL_TIME, updateVo.interval_duration);
+                    //请求更新服务时间
+                    SPUtils.getInstance().put(SPKeyGlobal.APP_LAST_CHECK_TIME, System.currentTimeMillis());
+                    long versionCode = Long.valueOf(StringUtils.getVersionCode(getContext()));
+                    CfLog.i("versionCode = " + versionCode);
+                    if (versionCode < updateVo.version_code) {
+                        //线上版本大于本机版本
+                        if (updateVo.type == 0) {
+                            //弱更
+                            if (versionCode >= vo.version_code_min) {
+                                showUpdate(true, updateVo); // 弱更
+                            } else {
+                                showUpdate(false, updateVo); // 强更
+                            }
 
-                    } else if (updateVo.type == 1) {
-                        //强更
-                        showUpdate(false, updateVo);
-                    } else if (updateVo.type == 2) {
-                        //热更
+                        } else if (updateVo.type == 1) {
+                            //强更
+                            showUpdate(false, updateVo);
+                        } else if (updateVo.type == 2) {
+                            //热更
+                        }
                     }
+                }else {
+                    CfLog.e("****************** App更新地址非法****************");
+                    ToastUtils.showError("App更新地址有误，请稍后再试");
                 }
+
             }
 
         });
@@ -659,6 +666,8 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         if (updateView != null && updateView.isShow()) {
             return;
         }
+        CfLog.e("showUpdate ----> " + vo.download_url);
+
         AppUpdateDialog dialog = new AppUpdateDialog(getContext(), isWeakUpdate, vo, new AppUpdateDialog.IAppUpdateCallBack() {
             @Override
             public void onUpdateCancel() {
