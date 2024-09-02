7cf91f3b2e6be6ce2f390c8e0952adf7bd8863e5
hqxc-arason <hqxc.arason@gmail.com>
2024-09-02 16:42:06 +0800

    把之前超前开发的需求合进来

---
+++ module-recharge/src/main/java/com/xtree/recharge/ui/widget/TipBindCardDialog.java
@@ -0,0 +1,56 @@
+package com.xtree.recharge.ui.widget;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.lxj.xpopup.core.CenterPopupView;
+import com.xtree.recharge.R;
+import com.xtree.recharge.databinding.DialogBindCardTipsBinding;
+
+public class TipBindCardDialog extends CenterPopupView {
+
+    private ICallBack mCallBack;
+    private DialogBindCardTipsBinding binding;
+
+    public TipBindCardDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public TipBindCardDialog(@NonNull Context context, ICallBack mCallBack) {
+        super(context);
+        this.mCallBack = mCallBack;
+    }
+
+    // 定义回调接口
+    public interface ICallBack {
+        void onClickConfirm(); // 当用户点击确认按钮时触发的回调方法
+    }
+
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        initView();
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.dialog_bind_card_tips;
+    }
+
+    private void initView() {
+        binding = DialogBindCardTipsBinding.bind(findViewById(R.id.ll_root));
+
+        // 设置确认按钮的点击事件
+        binding.tvwOk.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCallBack != null) {
+                    mCallBack.onClickConfirm(); // 调用回调接口的方法
+                }
+                dismiss(); // 关闭对话框
+            }
+        });
+    }
+}
