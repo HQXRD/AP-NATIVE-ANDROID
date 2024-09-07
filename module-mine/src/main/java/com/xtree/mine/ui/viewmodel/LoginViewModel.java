15ba464594ab357d5cab2b19710fab4e527b47c7
hqxc-daniel <hqxc.daniel@gmail.com>
2024-09-07 15:33:33 +0800

    首页 进入熊猫体育极速版概率性报错问题修复

--- module-mine/src/main/java/com/xtree/mine/ui/viewmodel/LoginViewModel.java
+++ module-mine/src/main/java/com/xtree/mine/ui/viewmodel/LoginViewModel.java
@@ -189,53 +189,6 @@ public class LoginViewModel extends BaseViewModel<MineRepository> {
                 });
         addSubscribe(disposable);
     }
-
-    public void getFBGameTokenApi() {
-        Disposable disposable = (Disposable) model.getApiService().getFBGameTokenApi()
-                .compose(RxUtils.schedulersTransformer()) //线程调度
-                .compose(RxUtils.exceptionTransformer())
-                .subscribeWith(new HttpCallBack<FBService>() {
-                    @Override
-                    public void onResult(FBService fbService) {
-                        SPUtils.getInstance().put(SPKeyGlobal.FB_TOKEN, fbService.getToken());
-                        SPUtils.getInstance().put(SPKeyGlobal.FB_API_SERVICE_URL, fbService.getForward().getApiServerAddress());
-                        KLog.e("========fbService.getToken()======" + fbService.getToken());
-                        //finish();
-                        getPMGameTokenApi();
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        super.onError(t);
-                    }
-                });
-        addSubscribe(disposable);
-    }
-
-    public void getPMGameTokenApi() {
-        Disposable disposable = (Disposable) model.getApiService().getPMGameTokenApi()
-                .compose(RxUtils.schedulersTransformer()) //线程调度
-                .compose(RxUtils.exceptionTransformer())
-                .subscribeWith(new HttpCallBack<PMService>() {
-                    @Override
-                    public void onResult(PMService pmService) {
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_TOKEN, pmService.getToken());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_API_SERVICE_URL, pmService.getApiDomain());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_IMG_SERVICE_URL, pmService.getImgDomain());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_USER_ID, pmService.getUserId());
-                        KLog.e("========pmService.getToken()======" + pmService.getToken());
-                        //finish();
-                        //liveDataLogin.setValue(null); // 登录成功,重新打开APP
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        super.onError(t);
-                    }
-                });
-        addSubscribe(disposable);
-    }
-
     public void clearCache() {
 
         SPUtils.getInstance().remove(SPKeyGlobal.USER_TOKEN);
