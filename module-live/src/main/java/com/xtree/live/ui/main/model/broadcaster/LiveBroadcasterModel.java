ee5690bf6e6289e1e6d13220aa386ec6fe875b2c
hqxc-seka <hqxc.seka@gmail.com>
2024-09-13 10:56:29 +0800

    【Android】【安卓】体育直播接入(HQAP2-4785)[主播列表\个性头像]

--- module-live/src/main/java/com/xtree/live/ui/main/model/broadcaster/LiveBroadcasterModel.java
+++ module-live/src/main/java/com/xtree/live/ui/main/model/broadcaster/LiveBroadcasterModel.java
@@ -2,5 +2,8 @@ package com.xtree.live.ui.main.model.broadcaster;
 
 import com.xtree.base.mvvm.recyclerview.BindModel;
 
+/**
+ * 主播列表Model
+ */
 public class LiveBroadcasterModel extends BindModel {
 }
