5fc2b8971693bde0bccf033bcde2ab8026cc4819
hqxc-arason <hqxc.arason@gmail.com>
2024-09-21 15:13:13 +0800

    充值 充值快捷金额样式优化

--- module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeFragment.java
+++ module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeFragment.java
@@ -622,6 +622,18 @@ public class RechargeFragment extends BaseFragment<FragmentRechargeBinding, Rech
             return;
         }
 
+        //银行卡绑定判断
+        if (vo.view_bank_card && vo.userBankList.isEmpty()) {
+
+            binding.llBindInfo.setVisibility(View.VISIBLE);
+            binding.tvwBindYhk.setVisibility(View.VISIBLE);
+
+            // 绑定YHK
+            CfLog.i("****** 绑定YHK");
+            toBindCard();
+            return;
+        }
+
         //支付宝和微信判断银行卡绑定信息(极速充值也需要)
         if (vo.paycode.contains("zfb")|| vo.paycode.contains("wx")||vo.paycode.contains(ONE_PAY_FIX)) {
             //if (vo.op_thiriframe_use && vo.userBankList.isEmpty() && vo.view_bank_card && !vo.phone_needbind) {
