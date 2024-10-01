de46a34cc934c4190ae9349c35dc60e1c048fc86
hqxc-seka <hqxc.seka@gmail.com>
2024-10-01 16:06:03 +0800

    银行卡充值-玩家绑定手机后未绑定银行卡就能进入充值页（722)[暂时放开针对银行卡绑卡逻辑]

--- module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeFragment.java
+++ module-recharge/src/main/java/com/xtree/recharge/ui/fragment/RechargeFragment.java
@@ -619,8 +619,7 @@ public class RechargeFragment extends BaseFragment<FragmentRechargeBinding, Rech
             toBindPhoneNumber();
             return;
         }
-        CfLog.e("vo.view_bank_card ==false " + vo.view_bank_card + " || vo.userBankList = " +vo.userBankList.size()) ;
-        //增加银行卡充值判断绑卡绑卡逻辑
+       /* //增加银行卡充值判断绑卡绑卡逻辑  暂时作废
         if (vo.view_bank_card && vo.userBankList.size() == 0) {
             binding.llBindInfo.setVisibility(View.VISIBLE);
             binding.tvwBindYhk.setVisibility(View.VISIBLE);
@@ -629,12 +628,12 @@ public class RechargeFragment extends BaseFragment<FragmentRechargeBinding, Rech
             CfLog.i("****** 绑定YHK");
             toBindCard();
             return;
-        }
+        }*/
 
         //支付宝和微信判断银行卡绑定信息
         if (vo.paycode.contains("zfb")|| vo.paycode.contains("wx")) {
             //if (vo.op_thiriframe_use && vo.userBankList.isEmpty() && vo.view_bank_card && !vo.phone_needbind) {
-            if (vo.view_bank_card ==false&& vo.userBankList.isEmpty()) {
+            if (vo.view_bank_card && vo.userBankList.isEmpty()) {
 
                 binding.llBindInfo.setVisibility(View.VISIBLE);
                 binding.tvwBindYhk.setVisibility(View.VISIBLE);
